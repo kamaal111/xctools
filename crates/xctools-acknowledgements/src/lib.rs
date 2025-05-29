30318e01e168b2608d447733bdd80c81118ad0de
Kamaal Farah <kamaal.f1@gmail.com>
2025-05-29 17:44:56 +0200

    Getting package license contents

--- crates/xctools-acknowledgements/src/lib.rs
+++ crates/xctools-acknowledgements/src/lib.rs
@@ -1,15 +1,71 @@
 use anyhow::{Context, Result, anyhow};
 use glob::glob;
 use std::{
-    env, fs,
+    collections::HashMap,
+    ffi::OsString,
     path::PathBuf,
     process::{Command, Stdio},
 };
 
 pub fn acknowledgements(app_name: &String, output: &String) -> Result<String> {
-    let result = find_derived_data_for_app(app_name);
-    println!("{:?}", result);
+    let packages_directory = get_packages_directory(app_name)?;
+    let packages_licenses = get_packages_licenses(&packages_directory);
+    println!("{:?}", packages_licenses);
     Ok(String::from(""))
+}
+
+fn get_packages_licenses(packages_directory: &PathBuf) -> Result<HashMap<OsString, String>> {
+    assert!(packages_directory.is_dir());
+
+    let packages_directory_contents = std::fs::read_dir(packages_directory)
+        .map_err(|e| {
+            anyhow!(format!(
+                "Failed to read packages directory contents; error='{}'",
+                e
+            ))
+        })?
+        .filter_map(|p| p.ok())
+        .filter(|e| e.path().is_dir());
+    let mut licenses: HashMap<OsString, String> = HashMap::new();
+    for content in packages_directory_contents {
+        let package_licenses: Vec<_> = std::fs::read_dir(content.path())
+            .map_err(|e| {
+                anyhow!(format!(
+                    "Failed to read packages directory contents; error='{}'",
+                    e
+                ))
+            })?
+            .filter_map(|d| d.ok())
+            .map(|d| d.path())
+            .filter(|p| {
+                if !p.is_file() {
+                    return false;
+                }
+
+                p.file_name()
+                    .and_then(|name| name.to_str())
+                    .map(|name_str| name_str.to_lowercase().contains("license"))
+                    .unwrap_or(false)
+            })
+            .collect();
+        let license_path = match package_licenses.first() {
+            None => continue,
+            Some(entry) => entry,
+        };
+        let license = std::fs::read_to_string(license_path)
+            .map_err(|e| anyhow!(format!("Failed to read license; error='{}'", e)))?;
+        licenses.insert(content.file_name(), license);
+    }
+
+    Ok(licenses)
+}
+
+fn get_packages_directory(app_name: &String) -> Result<PathBuf> {
+    let result = find_derived_data_for_app(app_name)?
+        .join("SourcePackages/checkouts")
+        .clone();
+
+    Ok(result)
 }
 
 fn find_derived_data_for_app(app_name: &String) -> Result<PathBuf> {
@@ -27,13 +83,16 @@ fn find_derived_data_for_app(app_name: &String) -> Result<PathBuf> {
             ))
         })
         .and_then(|matches| {
-            let mut paths: Vec<_> = matches.filter_map(|p| p.ok()).collect();
+            let mut paths: Vec<_> = matches
+                .filter_map(|p| p.ok())
+                .filter(|p| p.is_dir())
+                .collect();
             // Sort by last modified, the first in the list being latest updated
             paths.sort_by(|a, b| {
-                let a_modified = fs::metadata(a)
+                let a_modified = std::fs::metadata(a)
                     .and_then(|m| m.modified())
                     .unwrap_or(std::time::SystemTime::UNIX_EPOCH);
-                let b_modified = fs::metadata(b)
+                let b_modified = std::fs::metadata(b)
                     .and_then(|m| m.modified())
                     .unwrap_or(std::time::SystemTime::UNIX_EPOCH);
 
@@ -58,7 +117,7 @@ fn get_xcode_derived_data_base() -> Result<PathBuf> {
 }
 
 fn get_default_derived_data_base() -> Result<PathBuf> {
-    let result = env::home_dir()
+    let result = std::env::home_dir()
         .with_context(|| "Failed to load home directory")?
         .join("Library/Developer/Xcode/DerivedData");
 
